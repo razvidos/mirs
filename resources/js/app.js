425a4890c17f7d054e6121264bc8193076455b2b
ihor.solomianyi <ihor.solomianyi@appflame.com>
2022-11-12 10:21:35 +0200

    Vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-import './bootstrap';
+import {createApp} from "vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
+import axios, {isCancel, AxiosError} from 'axios';
+
+
+createApp(ExampleComponent).mount('#app');
+
+axios.post(import.meta.env.VITE_FIRE_BASE_URL + 'hotel.json', {
+    params: {
+        name: 'Hreshtyk Hotel', city: 'Kyiv',
+    }
+})
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
