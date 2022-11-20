10c941687ca194d61f183b50ad8b75f150f41863
ihor.solomianyi <ihor.solomianyi@appflame.com>
2022-11-20 21:55:35 +0200

    Setup PWA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,22 @@
 
     <title>Laravel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef"/>
+    <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
 </head>
 <body class="antialiased" id="app">
 <div class="app"></div>
+
+<script src="{{ asset('/sw.js') }}"></script>
+<script>
+    if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register("/sw.js").then(function (reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+</script>
 </body>
 
