57d178a18097e99b45eb6733c02552cae4a1f200
ihor.solomianyi <ihor.solomianyi@appflame.com>
2022-11-23 15:17:18 +0200

    refactor
    Signed-off-by: ihor.solomianyi <ihor.solomianyi@appflame.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('logo.PNG') }}">
-    <title>Laravel</title>
+@extends('laouts.mainApp')
+
+@section('vite')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- PWA  -->
-    <meta name="theme-color" content="#6777ef"/>
-    <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
-    <link rel="manifest" href="{{ asset('/manifest.json') }}">
-    <script src="{{ asset('/sw.js') }}"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            if (!navigator.serviceWorker.controller) {
-                navigator.serviceWorker.register("/sw.js").then(function (reg) {
-                    console.log("Service worker has been registered for scope: " + reg.scope);
-                });
-            }
-        });
-    </script>
-</head>
-<body class="antialiased" id="app">
-<div class="app"></div>
-</body>
-
+@endsection
