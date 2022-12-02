26f5698aa917333ae0ebdbdda83dde8c17370bd5
ihor.solomianyi <ihor.solomianyi@appflame.com>
2022-12-03 00:04:03 +0200

    test
    Signed-off-by: ihor.solomianyi <ihor.solomianyi@appflame.com>

---
+++ resources/js/components/calc/CalcInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <input type="text">
+</template>
+
+<script>
+export default {
+    name: "CalcInput"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/calc/Keyboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-btn>C</v-btn>
+            </v-col>
+            <v-col>
+                +
+            </v-col>
+            <v-col>
+                -
+            </v-col>
+            <v-col>
+                *
+            </v-col>
+            <v-col>
+                /
+            </v-col>
+            <v-col>
+                %
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "Keyboard"
+}
+</script>
+
+<style scoped>
+
+</style>
