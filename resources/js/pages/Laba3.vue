6e201ca3ed656566c4f91f11720706113ece3e8c
ihor.solomianyi <ihor.solomianyi@appflame.com>
2022-12-04 01:15:26 +0200

    Laba 3 калькулятор
    Signed-off-by: ihor.solomianyi <ihor.solomianyi@appflame.com>

--- resources/js/pages/Laba3.vue
+++ resources/js/pages/Laba3.vue
@@ -1,23 +1,57 @@
 <template>
-    <header>Laba3</header>
+    <v-container>
+        <v-sheet>
+            <v-col cols="6">
+                <v-card-title v-text="$trans.get('app.nav.laba3')"></v-card-title>
+                <v-text-field
+                    label="first number"
+                    v-model="firstNumber"
+                >
+                </v-text-field>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                    label="second number"
+                    v-model="secondNumber"
+                >
+                </v-text-field>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                    label="result"
+                    v-model="result"
+                >
+                </v-text-field>
+            </v-col>
+            <v-card-actions class="ma-3">
+
+                <v-row>
+                    <v-btn v-for="operator in operators"
+                           :active="calcOperator===operator"
+                           @click="calcOperator=operator">{{ operator }}</v-btn>
+                </v-row>
+            </v-card-actions>
+        </v-sheet>
+    </v-container>
 </template>
 
 <script>
-
-// import Keyboard from "./calc/Keyboard.vue";
-// import CalcInput from "./calc/CalcInput.vue";
-
 export default {
     name: "Application",
-    // components: {Keyboard, CalcInput},
     data() {
         return {
-            hotelList: [],
+            firstNumber: null,
+            secondNumber: null,
+            calcOperator: null,
+            operators: ['+', '-', '*', '/', '%', '**']
         }
     },
-    methods: {
-        enemyUpdate(list) {
-            this.hotelList = list;
+    computed: {
+        result() {
+            if (!this.firstNumber || !this.secondNumber || !this.calcOperator) {
+                return null;
+            }
+            return eval(this.firstNumber + this.calcOperator + this.secondNumber);
         }
     },
 }
